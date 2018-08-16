5bbd48e09b35226938e927ca7a12b758b5509be3
Brooks Swinnerton <bswinnerton@gmail.com>
2018-08-15 22:11:37 -0400

    Remove setup file
    This isn't needed! Docker Compose automatically builds new volumes.

--- grafana/setup.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-[ -f .env ] || echo ".env file not present"; exit 1
-
-echo "Creating Grafana configuration volume..."
-sudo docker volume create grafana-config
-
-echo "Creating Grafana data volume..."
-sudo docker volume create grafana-data
